8fe30ccd8418186409379b479aa7e5e075b485ad
T-L-wxp <43870404+T-L-wxp@users.noreply.github.com>
2024-08-28 18:28:13 +0800

    Add files via upload

--- wiki/src/router/index.js
+++ wiki/src/router/index.js
@@ -1,13 +1,19 @@
-import Vue from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../components/HomePage.vue'
+import XSPage from '../components/Pages/XSPage.vue'
 
-Vue.use(VueRouter)
 const routes = [
     {
         path: '/',
-        component: HomePage,
-        children: [
-
-        ]
+        component: HomePage
+    },
+    {
+        path: '/XS',
+        component: XSPage
     }
 ]
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+  });
+export default router;
